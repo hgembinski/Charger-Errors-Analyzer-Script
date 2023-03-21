33a811ef843a8169a2c59d000d30825664940736
Haiden <hgembinski@gmail.com>
2023-03-21 15:33:50 -0400

    Fixed filtering of selected error codes

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import pandas as pd
 # Read in CSV file
 df = pd.read_csv('error_log_last7.csv')
 
+#error codes to be ommitted
+l2_codes = ['1','2']
+dcfc_codes = ['1', '2', '62', '63', '64', '74', '188', '302', '303']
+
 # Create function to aggregate codes and remove duplicates
 def unique_codes(x):
-    return ', '.join(set(x))
+    return ','.join(set(x))
+
+def filter_errors(entry):
+    codes = entry['code'].split(',')
+    if entry['type'] == 'DCFC' and set(codes).issubset(dcfc_codes):
+        print(entry['type'] + ' ' + entry['name'] + ' ' + entry['code'])
+        return False
+    elif set(codes).issubset(dcfc_codes):
+        print(entry['type'] + ' ' + entry['name'] + ' ' + entry['code'])
+        return False
+    else:
+        return True
 
 # Create new dataframe to aggregate by serial
-serial_df = df.groupby('serial').agg({
+aggregate_df = df.groupby('serial').agg({
     'type': 'first',
     'company': 'first',
     'serial': 'first',
@@ -16,13 +31,8 @@ serial_df = df.groupby('serial').agg({
     'code': unique_codes
 })
 
-# If a charger ONLY has certain errors logged, remove it from the output df
-# Mostly to filter timeout codes and vehicle errors
-serial_df = serial_df[~(
-    (serial_df['code'].str.contains('1|2') & (serial_df['code'].str.len() <= 3)) | # remove serials with only codes 1 or 2
-    ((serial_df['type'] == 'DCFC') & serial_df['code'].str.contains('62|63|74|188|302|303') & (serial_df['code'].str.len() <= 9)) # remove serials with DCFC and only codes 62, 63, 74, 188, 302, 303
-)]
+output_df = aggregate_df[aggregate_df.apply(filter_errors, axis=1)]
 
 # Output aggregated dataframe to CSV file
-serial_df.to_csv('output.csv', index=False)
+output_df.to_csv('output.csv', index=False)
 print("Done.")
